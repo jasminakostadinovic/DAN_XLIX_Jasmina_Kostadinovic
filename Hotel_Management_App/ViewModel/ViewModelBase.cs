fb8f27ba331dcf60f37318102cf52ac354fc5479
jasminakostadinovic <jasmina.kostadinovic26@gmail.com>
2020-08-04 11:07:20 +0200

    MVVM folder structure

---
+++ Hotel_Management_App/ViewModel/ViewModelBase.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace Hotel_Management_App.ViewModel
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
